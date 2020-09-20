277d6f6420ef292c44d3e4eb53a5d2425392b698
Oscar Miticanoy <oscarmiticanoy@gmail.com>
2020-09-20 05:53:59 -0500

    Add project files.

---
+++ PruebaTecnicaParteUno/Matriz.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PruebaTecnicaParteUno
+{
+    class Matriz
+    {
+        private int [,] matriz;
+        private int n;
+        public Matriz(int n)
+        {
+            this.n = n;
+            matriz = new int[n,n];
+        }
+
+        public void llenarAleatorio()
+        {
+            var seed = Environment.TickCount;
+            var random = new Random(seed);
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matriz[i,j] = random.Next(0, 100);
+                }
+            }
+        }
+
+        public void imprimirMatriz()
+        {
+            for (int i = 0; i < n; i++)
+            {
+
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(matriz[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        public int diagonalPrincipal()
+        {
+            int suma = 0;
+            for (int i = 0; i < n; i++)
+            {
+                suma = suma + matriz[i, i];
+            }
+            return suma;
+        }
+
+        public int diagonalInvesa()
+        {
+            int suma = 0;
+            for (int i = 0; i < n; i++)
+            {
+                suma = suma + matriz[i, (n-1)-i];
+            }
+            return suma;
+        }
+
+        public int maximo()
+        {
+            int max = 0; // suponiendo que cero es el menor numero que tendremos
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (matriz[i,j]>max)
+                    {
+                        max = matriz[i, j];
+                    }
+                }
+            }
+            return max;
+        }
+
+        public int minimo()
+        {
+            int min = 100; // suponiendo que 100 es el mayor numero que tendremos
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (matriz[i, j] < min)
+                    {
+                        min = matriz[i, j];
+                    }
+                }
+            }
+            return min;
+        }
+    }
+}
---
+++ PruebaTecnicaParteUno/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+
+namespace PruebaTecnicaParteUno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matriz m;
+            int n;
+            Console.WriteLine("Matriz de nxn");
+            Console.WriteLine("valor de n: ");
+            n = int.Parse(Console.ReadLine());
+            if ( n>=0 && n <= 100 )
+            {
+                m = new Matriz(n);
+                m.llenarAleatorio();
+                Console.WriteLine("\n\nMatriz actual\n");
+                m.imprimirMatriz();
+                Console.WriteLine("\n\nResultados obtenidos\n");
+                Console.WriteLine("La diagonal principal suma "+m.diagonalPrincipal()+"\n");
+                Console.WriteLine("La diagonal inversa suma " + m.diagonalInvesa() + "\n");
+                Console.WriteLine("El numero maximo es " + m.maximo() + "\n");
+                Console.WriteLine("El numero minimo es  " + m.minimo() + "\n");
+            }
+            else
+            {
+                Console.WriteLine("valor de n debe estar en el rango 0 - 100");
+            }
+        }
+    }
+}
